b1166719c46314771b3800c899563e9ccc4163e7
KovalevSergei <ckv51@mail.ru>
2022-06-17 16:33:22 +0300

    wb

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -4,10 +4,11 @@ import {validationResult}  from 'express-validator'
 export const inputValidation=(req: Request, res: Response, next: NextFunction)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const errorsArray=errors.array({onlyFirstError:true}).map((error)=>{ 
-        return {message: error.msg, field: error.param} 
-       })
-      res.status(400).json({ errorsMessages: errorsArray });
+      //const errorsArray=errors.array({onlyFirstError:true}).map((error)=>{ 
+      //  return {message: error.msg, field: error.param} 
+        
+       
+      res.status(400).json({ errorsMessages: errors.array() });
     }else{
         next()
     }
