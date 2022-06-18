fc3c545f5eeedf2508f34e467bfcaa8e24fed567
KovalevSergei <ckv51@mail.ru>
2022-06-18 13:28:21 +0300

    eg

--- dist/test.js
+++ dist/test.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const db_1 = require("./repositories/db");
 const bloggers_routes_1 = require("./route/bloggers-routes");
 const posts_router_1 = require("./route/posts-router");
 //import { ReadableStreamBYOBRequest } from 'stream/web';
@@ -15,17 +14,6 @@ const port = process.env.PORT || 3000;
 app.use(body_parser_1.default.json());
 app.use("/bloggers", bloggers_routes_1.bloggersRouter);
 app.use("/posts", posts_router_1.postsRouter);
-app.delete('/posts/:id', (req, res) => {
-    const id = +req.params.id;
-    const ind = db_1.posts.findIndex(v => v.id === id);
-    if (ind === -1) {
-        res.sendStatus(404);
-    }
-    else {
-        db_1.posts.splice(ind, 1);
-        res.sendStatus(204);
-    }
-});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/test.ts
+++ src/test.ts
@@ -20,16 +20,6 @@ app.use("/posts",postsRouter)
 
  
 
- app.delete('/posts/:id', (req : Request, res : Response)=>{
-  const id= +req.params.id;
-  const ind=posts.findIndex(v => v.id=== id)
-  if (ind===-1){
-    res.sendStatus(404)
-  }else{
-    posts.splice(ind,1)
-    res.sendStatus(204)
-  }
- })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
