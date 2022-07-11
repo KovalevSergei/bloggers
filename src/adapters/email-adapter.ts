c8ff3b45b15f0bf3be07fba49e00180390c6ca45
KovalevSergei <ckv51@mail.ru>
2022-07-11 10:11:45 +0300

    sc

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -15,7 +15,6 @@ export const emailAdapter = {
       from: "Server <sss3232829@gmail.com>", // sender address
       to: email, // list of receivers
       subject: subject, // Subject line
-      text: code,
       html: `<div>https://some-front.com/confirm-registration?code=${code}</div>`, // html body
     });
 
