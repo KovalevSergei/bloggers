d0fac5f5d2582983fdaab5632272e290f41afcf7
KovalevSergei <ckv51@mail.ru>
2022-07-11 08:48:49 +0300

    sc

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -12,7 +12,7 @@ export const emailAdapter = {
   async sendEmail(email: string, subject: string, code: string) {
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: "Server <s3232829@gmail.com>", // sender address
+      from: "Server <sss3232829@gmail.com>", // sender address
       to: email, // list of receivers
       subject: subject, // Subject line
       html: `<div>https://some-front.com/confirm-registration?code=${code}</div>`, // html body
