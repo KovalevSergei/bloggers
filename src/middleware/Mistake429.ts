a70db06fe70b6694e2d490faa0e382b37fac6afb
KovalevSergei <ckv51@mail.ru>
2022-07-20 16:14:54 +0300

    df

--- src/middleware/Mistake429.ts
+++ src/middleware/Mistake429.ts
@@ -9,7 +9,6 @@ export const Mistake429 = async (
   next: NextFunction
 ) => {
   const point = req.method + req.originalUrl;
-  console.log(point);
 
   const ip = req.ip;
 
@@ -21,7 +20,6 @@ export const Mistake429 = async (
   await ipCollection.insertOne({ ...newCRUD, _id: new ObjectId() });
   const fromData = new Date();
   fromData.setSeconds(fromData.getSeconds() - 10);
-  console.log(fromData);
   const totalCount = await ipCollection.count({
     point: point,
     ip: ip,
