2e4ad4154e2609ea27de8a72786608c89901623b
KovalevSergei <ckv51@mail.ru>
2022-08-16 14:13:09 +0300

    sx

--- src/repositories/testing-repository.ts
+++ src/repositories/testing-repository.ts
@@ -6,6 +6,7 @@ import {
   ipModel,
   likeCommentsModel,
   likePostsModel,
+  tokenModel,
 } from "./db";
 
 export const testingRepository = {
@@ -13,6 +14,7 @@ export const testingRepository = {
     await bloggersModel.deleteMany({});
     await usersModel.deleteMany({});
     await commentsModel.deleteMany({});
+    await tokenModel.deleteMany({});
     await postsModel.deleteMany({});
     await ipModel.deleteMany({});
     await likeCommentsModel.deleteMany({});
